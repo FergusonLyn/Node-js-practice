56059c8e2f9a743770f37690ef94bca1637e5169
FergusonLyn <psky730@gmail.com>
2024-07-10 19:10:07 +0100

    first commit

---
+++ server2.js
@@ -0,0 +1,30 @@
+import { createServer } from 'http' ;
+const PORT = process.env.PORT || 8000;
+
+const users = [
+    {id:1 , name: 'John Doe'},
+    {id:2 , name: 'Jane Doe'},
+    {id:3 , name: 'Jay Doe'},
+];
+
+
+const server = createServer((req,res) => {
+    if (req.url === '/api/users' && req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(users));
+        res.end();
+    } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 404;
+        res.write(JSON.stringify({message: 'Route not found'}));
+        res.end();
+    };
+
+});
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+});
